8e9942c77637ac18ad3162aa7c91cc4c40b9967d
Francisco de Borja Viera Prieto <borja33357@gmail.com>
2022-10-26 07:55:18 +0200

    la03, hasta el ejercicio 2.1

---
+++ src/la03/ej2/EjemploMuestraPrimosEnVector.java
@@ -0,0 +1,146 @@
+package la03.ej2;
+
+// ===========================================================================
+public class EjemploMuestraPrimosEnVector {
+// ===========================================================================
+
+  // -------------------------------------------------------------------------
+  public static void main( String args[] ) {
+    int     numHebras, vectOpt;
+    boolean option = true;
+    long    t1, t2;
+    double  ts, tc, tb, td;
+
+    // Comprobacion y extraccion de los argumentos de entrada.
+    if( args.length != 2 ) {
+      System.err.println( "Uso: java programa <numHebras> <vectorOption>" );
+      System.exit( -1 );
+    }
+    try {
+      numHebras = Integer.parseInt( args[ 0 ] );
+      vectOpt   = Integer.parseInt( args[ 1 ] );
+      if ( ( vectOpt != 0 ) && ( vectOpt != 1 ) ) {
+        System.out.println( "ERROR: vectorOption should be 0 or 1.");
+        System.exit( -1 );
+      } else {
+        option = (vectOpt == 0);
+      }
+    } catch( NumberFormatException ex ) {
+      numHebras = -1;
+      System.out.println( "ERROR: Argumentos numericos incorrectos." );
+      System.exit( -1 );
+    }
+
+    //
+    // Eleccion del vector de trabajo
+    //
+    VectorNumeros vn = new VectorNumeros (option);
+    long vectorNumeros[] = vn.vector;
+
+    //
+    // Implementacion secuencial.
+    //
+    System.out.println( "" );
+    System.out.println( "Implementacion secuencial." );
+    t1 = System.nanoTime();
+    for( int i = 0; i < vectorNumeros.length; i++ ) {
+      if( esPrimo( vectorNumeros[ i ] ) ) {
+        System.out.println( "  Encontrado primo: " + vectorNumeros[ i ] );
+      }
+    }
+    t2 = System.nanoTime();
+    ts = ( ( double ) ( t2 - t1 ) ) / 1.0e9;
+    System.out.println( "Tiempo secuencial (seg.):                    " + ts );
+/*
+    //
+    // Implementacion paralela ciclica.
+    //
+    System.out.println( "" );
+    System.out.println( "Implementacion paralela ciclica." );
+    t1 = System.nanoTime();
+    // Gestion de hebras para la implementacion paralela ciclica
+    // ....
+    t2 = System.nanoTime();
+    tc = ( ( double ) ( t2 - t1 ) ) / 1.0e9;
+    System.out.println( "Tiempo paralela ciclica (seg.):              " + tc );
+    System.out.println( "Incremento paralela ciclica:                 " + ... );
+    //
+    // Implementacion paralela por bloques.
+    //
+    // ....
+    //
+    // Implementacion paralela dinamica.
+    //
+    // ....
+*/
+  }
+
+  // -------------------------------------------------------------------------
+  static boolean esPrimo( long num ) {
+    boolean primo;
+    if( num < 2 ) {
+      primo = false;
+    } else {
+      primo = true;
+      long i = 2;
+      while( ( i < num )&&( primo ) ) { 
+        primo = ( num % i != 0 );
+        i++;
+      }
+    }
+    return( primo );
+  }
+}
+
+// ===========================================================================
+class VectorNumeros {
+// ===========================================================================
+  long    vector[];
+
+  // -------------------------------------------------------------------------
+  public VectorNumeros (boolean caso) {
+    if (caso) {
+      vector = new long [] {
+      200000033L, 200000039L, 200000051L, 200000069L,
+      200000081L, 200000083L, 200000089L, 200000093L,
+      200000107L, 200000117L, 200000123L, 200000131L,
+      200000161L, 200000183L, 200000201L, 200000209L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L
+			};
+    } else {
+      vector = new long [] {
+      200000033L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000039L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000051L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000069L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000081L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000083L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000089L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000093L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000107L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000117L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000123L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000131L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000161L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000183L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000201L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+      200000209L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L 
+      };
+    }
+  }
+}
+
